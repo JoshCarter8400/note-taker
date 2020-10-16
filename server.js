a36ffd9c59e834d7c8e5b96ba03c9a23c97a0b3c
Josh Carter <joshcarter8400@gmail.com>
2020-10-15 22:05:47 -0400

    saving tasks

--- server.js
+++ server.js
@@ -1,22 +1,33 @@
-const htmlRoutes = require("./routes/htmlRoutes");
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
-const router = require("express").Router();
-
+var newNote = require("./db/db.json");
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
 app.use(express.static("public"));
+// html routes
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
-app.use("/", htmlRoutes);
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
 
-// const { notes } = require("./Develop/db/db.json");
+app.get("/api/notes", (req, res) => {
+  res.json(newNote);
+});
 
+app.post("/api/notes", (req, res) => {
+  req.body.id;
+  newNote.push(req.body);
+  res.json(newNote);
+});
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
